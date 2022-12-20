5986260f9dba1da3a98543fcbdbdc6b8076cd1ba
Leojahir <91861439+Leojahir@users.noreply.github.com>
2022-12-20 18:04:22 -0300

    Se sube dato para servicio json

---
+++ data1.php
@@ -0,0 +1,37 @@
+[
+  {
+    "codigo": 1,
+    "marca": "Asus",
+    "modelo": "AMD RadeonRX 6500XT O4GB Dual",
+    "precio": 200000,
+    "cantidad": 5
+  },
+  {
+    "codigo": 2,
+    "marca": "Asus",
+    "modelo": "NVIDIA GeForce GTX1650 O4GB Dual ",
+    "precio": 250000,
+    "cantidad": 10
+  },
+  {
+    "codigo": 3,
+    "marca": "Logitech",
+    "modelo": "Mouse Gamer G502 X PLUS Wireless",
+    "precio": 130000,
+    "cantidad": 15
+  },
+  {
+    "codigo": 4,
+    "marca": "Razer",
+    "modelo": "Mouse Gamer Basilisk V",
+    "precio": 80000,
+    "cantidad": 5
+  },
+  {
+    "codigo": 5,
+    "marca": "Samsung",
+    "modelo": "Monitor Gamer 27" Odyssey G4 ",
+    "precio": 330000,
+    "cantidad": 3
+  }
+]
